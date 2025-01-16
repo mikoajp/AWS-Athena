629e0a7ca73ca06d00e41e03dcf6767a0ebb2d9e
Mikołaj Przybysz <145334739+mikoajp@users.noreply.github.com>
2025-01-16 13:34:40 +0100

    Add files via upload

---
+++ docker/scripts/entrypoint.sh
@@ -0,0 +1,17 @@
+OS=$(uname -s)
+case "$OS" in
+    Linux*)
+        echo "Detected Linux"
+        source /docker-scripts/init-linux.sh
+        ;;
+    MINGW*|CYGWIN*)
+        echo "Detected Windows"
+        source /docker-scripts/init-windows.sh
+        ;;
+    *)
+        echo "Unknown OS: $OS"
+        exit 1
+        ;;
+esac
+
+exec "$@"
