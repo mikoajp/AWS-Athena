b8630eb48d9b219155f4de5080bd369990be2dd8
mikoajp <mikoajmicht@gmail.com>
2025-01-17 14:49:04 +0100

    feat: init.sh fixy

--- docker/scripts/init-windowsh.sh
+++ docker/scripts/init-windowsh.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ ! -d "vendor" ]; then
+    composer install --no-interaction --no-progress
+fi
+
 mkdir -p storage/framework/sessions
 mkdir -p storage/framework/views
 mkdir -p storage/framework/cache
@@ -12,3 +16,7 @@ chmod -R 777 bootstrap/cache
 find . -type f -name "*.php" -exec dos2unix {} \;
 find . -type f -name "*.json" -exec dos2unix {} \;
 find . -type f -name "*.sh" -exec dos2unix {} \;
+
+php artisan migrate
+
+exec php artisan serve --host=0.0.0.0 --port=8000
