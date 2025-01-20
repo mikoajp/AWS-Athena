71034af0722d5ccb17c065fca291ac6f6ede3828
Mikołaj Przybysz <145334739+mikoajp@users.noreply.github.com>
2025-01-20 19:38:13 +0100

    Add files via upload

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-OS=$(uname -s)
-case "$OS" in
-    Linux*)
-        echo "Detected Linux"
-        source /docker-scripts/init-linux.sh
-        ;;
-    MINGW*|CYGWIN*)
-        echo "Detected Windows"
-        source /docker-scripts/init-windows.sh
-        ;;
-    *)
-        echo "Unknown OS: $OS"
-        exit 1
-        ;;
-esac
+php artisan migrate
 
-exec "$@"
+apache2-foreground
